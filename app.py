0ef1cdbf7e9c4e29b96b47f485593e142c155362
Your Name <Amberlanqinyun@gmail.com>
2024-10-10 21:45:00 +1300

    updated

--- app.py
+++ app.py
@@ -49,5 +49,5 @@ def meditation_end():
     return render_template('meditation_end.html')
 
 if __name__ == '__main__':
-    port = int(os.getenv("PORT", 8080))
+    port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port) 
