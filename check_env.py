9228e9034077d021f0209a403874442b9ae64aa3
Your Name <Amberlanqinyun@gmail.com>
2024-10-12 12:20:44 +1300

    updated & removed cache to release space

--- check_env.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
-
-# Print the Google OAuth credentials
-print("GOOGLE_CLIENT_ID:", os.getenv('GOOGLE_CLIENT_ID'))
-print("GOOGLE_CLIENT_SECRET:", os.getenv('GOOGLE_CLIENT_SECRET'))
